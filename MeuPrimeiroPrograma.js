52ae03edb57a401941c557f384fbaa1558af0939
LUCAS HENRIQUE DE OLIVEIRA <henrique.oliveira03@escola.pr.gov.br>
2025-08-29 10:44:34 +0000

    conversao de tipos

---
+++ MeuPrimeiroPrograma.js
@@ -0,0 +1,12 @@
+console.log("meu primeiro programa: trabalhando com variáveis")
+
+const idade = 16;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSoma = + 8;
+console.log(idadeSoma);
---
+++ trabalhandocomVariaveis.js
@@ -0,0 +1,11 @@
+//console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 16;
+
+const Idade = 13;
+
+const nome = "Lucas";
+
+console.log(nome);
