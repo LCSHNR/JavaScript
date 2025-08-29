1502d9ea30add0438cb57012c3fee8b44fcfe4c6
LUCAS HENRIQUE DE OLIVEIRA <henrique.oliveira03@escola.pr.gov.br>
2025-08-29 12:23:25 +0000

    lista

---
+++ JavaScript/atribuicaoVariaveis.js
@@ -0,0 +1,17 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const primeiroNome = "Lucas";
+const sobrenome = " Henrique de Oliveira";
+
+console.log(`Meu Nome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0;
+contador = contador + 1;
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; // declarando variáveis
+idade = 16; // atribuindo valor
+idade = idade + 1;
+console.log(idade);
